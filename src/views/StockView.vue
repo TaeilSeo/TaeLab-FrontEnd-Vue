b8098d01dbd6e8690a4b77c4e5a55ec8deff0cac
seowju9273@gmail.com <seowju9273@gmail.com>
2022-05-18 23:57:29 +0900

    Apply bootstrap-vue card to stock view

--- src/views/StockView.vue
+++ src/views/StockView.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    <section>
-      <h2>관심 종목</h2>
-      <ul>
-        <li v-for="(stock, index) in stocks" :key="index">
-          {{ stock.symbol }}
-        </li>
-      </ul>
-    </section>
+  <div class="container container-padding">
+    <div class="container-inner">
+      <section>
+        <h2 class="pt-4 pb-4">관심 종목</h2>
+        <ul>
+          <li v-for="(stock, index) in stocks" :key="index">
+            <b-card :header="stock.symbol" class="mt-4 mb-4">
+              <b-card-text>{{ stock.company }}</b-card-text>
+            </b-card>
+          </li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
